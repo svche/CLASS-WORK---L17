98fef36e96e152af56eba8f891dce91cdadd2aaf
Станислав <@->
2017-09-25 15:50:33 +0300

    Add project files.

---
+++ CLASS WORK - L17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_WORK___L17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
